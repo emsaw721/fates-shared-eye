4e0c26bd10004ed3701e4b622d86f03b7642f74a
Emily Hill <emhillmed2021@gmail.com>
2022-11-10 13:25:49 -0600

    finished rough draft of post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 
 const router = require('express').Router();
+const { runInNewContext } = require('vm');
 const sequelize = require('../../config/connection'); 
 const { Post, User, Comment, Vote } = require('../../models');
 const withAuth = require('../../utils/auth'); 
@@ -60,3 +61,71 @@ router.get('/:id', (req,res) => {
         res.status(500).json(err); 
     })
 }); 
+
+// create a post on 
+router.post('/', withAuth, (req,res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.session.user_id
+    }).then(createdPost => res.json(createdPost))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+      })
+});
+
+//updates the vote numbers 
+router.put('/upvote', withAuth, (req,res) => {
+    //upvote is custom static method created in Post.js line 5 to line 25
+    // then pass session id along with all destructured properties in req.body 
+    Post.upvote({ ...req.body, user_id: req.session.user_id}, {Vote, Comment, User})
+    .then(updatedVote => res.json(updatedVote))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+}); 
+
+
+//updates post at a certain id 
+router.put ('/:id', withAuth, (req,res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: { id: req.params.id }
+        }
+    ).then(updatedPost => {
+        if(!updatedPost) {
+            res.status(404).json({message: 'No post found for this id!'})
+            return; 
+        }
+        res.json(updatedPost)
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err); 
+    })
+}); 
+
+
+router.delete('/:id', withAuth, (req,res) => {
+    Post.destroy(
+        {
+            where: { id: req.params.id }
+        }
+    ).then(deletedPost => {
+        if(!deletedPost){
+            res.status(404).json({message: 'No post found for this id!'})
+            return; 
+        }
+        res.json(deletedPost); 
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err); 
+    })
+});
+
+module.exports = router; 
+
