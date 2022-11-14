91f0821713c85893ce87a18ee42482986d4b49c7
Emily Hill <emhillmed2021@gmail.com>
2022-11-14 14:10:34 -0600

    it is saying that findByPK is not a function

--- controllers/dashboard-rooutes.js
+++ controllers/dashboard-rooutes.js
@@ -8,7 +8,6 @@ router.get('/', withAuth, (req,res) => {
     console.log(req.session)
 
     Post.findAll({
-        where: { user_id: req.session.user_id},
         attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         include: [
             {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,6 @@ const {Post, User, Comment} = require('../models');
 // get all posts for homepage 
 router.get('/', (req,res) => {
     Post.findAll({
-        where: { user_id: req.session.user_id},
         attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'] ],
         include: [
             {
